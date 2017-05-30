46e65bc001fa7a0430bbfd8e42dba5af96e0b331
rod nolan <rodn@dev6.com>
2017-05-30 17:50:03 -0400

    configured native project for my Facebook Posterific app

--- posterific-basic/android/app/src/main/java/com/posterific/MainApplication.java
+++ posterific-basic/android/app/src/main/java/com/posterific/MainApplication.java
@@ -2,6 +2,8 @@ package com.posterific;
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
 import com.facebook.react.ReactApplication;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
@@ -14,6 +16,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost =
     new ReactNativeHost(this) {
@@ -27,7 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
       protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
+          new FBSDKPackage(mCallbackManager),
           new RNViewShotPackage(),
           new PickerPackage()
         );
@@ -42,5 +50,6 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
       super.onCreate();
+      FacebookSdk.sdkInitialize(getApplicationContext());
   }
 }
