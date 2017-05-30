7c6cd381d6562832151ac7b819dbc4a71fad3796
rod nolan <rodn@dev6.com>
2017-05-30 14:51:23 -0400

    installed and linked react-native-fbsdk

--- posterific-basic/android/app/src/main/java/com/posterific/MainApplication.java
+++ posterific-basic/android/app/src/main/java/com/posterific/MainApplication.java
@@ -3,6 +3,7 @@ package com.posterific;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(),
           new RNViewShotPackage(),
           new PickerPackage()
         );
